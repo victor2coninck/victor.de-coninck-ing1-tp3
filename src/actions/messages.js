8e3b0c68fba549d63af47f4335ebd46d3922e304
Victor de Coninck <victor.de-coninck@epita.fr>
2019-06-05 17:15:33 +0200

    adding german date (for fun) and hh:mm:ss

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,11 +1,11 @@
 import { ADD_MESSAGE } from "../constants/messages";
 
 let nextMessageId = 0;
-
-export const add = (text, userName) => ({
+let options = {weekday: "long", year: "numeric", month: "long", day: "numeric", hour:"2-digit", minute:"2-digit", second:"2-digit"};
+export const add = (text) => ({
   type: ADD_MESSAGE,
   id: nextMessageId++,
   text,
-  userName,
-  date: new Date().toLocaleDateString()
+  userName:"Usernem",
+  date: new Date().toLocaleDateString("de-DE", options)
 });
